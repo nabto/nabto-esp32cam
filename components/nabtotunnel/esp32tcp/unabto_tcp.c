0c4f028483c08c6a8ca95a89923a93db16ebcb55
Carsten Gregersen <crg@nabto.com>
2020-07-22 16:27:51 +0200

    Corrected wrong directory reference for unabto Corrected a define that is not missing anymore

--- components/nabtotunnel/esp32tcp/unabto_tcp.c
+++ components/nabtotunnel/esp32tcp/unabto_tcp.c
@@ -7,7 +7,9 @@
 #include <fcntl.h>
 #include <errno.h>
 
+#ifndef MSG_NOSIGNAL
 #define MSG_NOSIGNAL 0
+#endif
 
 unabto_tcp_status unabto_tcp_read(struct unabto_tcp_socket* sock, void* buf, const size_t len, size_t* read) {
     int status;
